57e0e8412a47450a0e45346103a7f78b95b87196
Alex Olivas <alexolivas@users.noreply.github.com>
2018-02-05 18:30:19 -0500

    Update env.sh

--- bash-git-prompt/env.sh
+++ bash-git-prompt/env.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 # enable bash-git-prompt if installed via homebrew
+if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+  GIT_PROMPT_THEME="Custom"
+  __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+  source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+fi
