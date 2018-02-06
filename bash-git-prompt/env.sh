3d1ea49b41630cc33a0583bb6023f2e88e98ccc0
Alex Olivas <alex.olivas11@gmail.com>
2018-02-06 13:39:16 -0500

    Copied  working .git-prompt-colors.sh from my home directory and converted it to symlink so that it works with the dotfiles project

--- bash-git-prompt/env.sh
+++ bash-git-prompt/env.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 # enable bash-git-prompt if installed via homebrew
-if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
-    GIT_PROMPT_THEME="AlexTheme"
-    __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
-    source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+if [ -f "$(brew --prefix bash-git-prompt)/share/gitprompt.sh" ]; then
+    GIT_PROMPT_THEME="Custom"
+    __GIT_PROMPT_DIR=$(brew --prefix bash-git-prompt)/share
+    source "$(brew --prefix bash-git-prompt)/share/gitprompt.sh"
 fi
