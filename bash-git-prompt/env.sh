9d1aeea18b8b26720322f547cbb25cd8a9cc80a9
Alex Olivas <alex.olivas11@gmail.com>
2018-02-06 10:47:44 -0500

    Fixed bash-git-prompt error: them name was changed from "custom"

--- bash-git-prompt/env.sh
+++ bash-git-prompt/env.sh
@@ -1,6 +1,6 @@
 # enable bash-git-prompt if installed via homebrew
 if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
-    GIT_PROMPT_THEME="Custom"
+    GIT_PROMPT_THEME="AlexTheme"
     __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
     source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
 fi
